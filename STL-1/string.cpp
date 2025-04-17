cf5f5f4a6988e752f5c7a6868273b2db68a19103
Uttam <uttamrohit4545@gmail.com>
2025-04-18 02:36:16 +0600

    string concept

---
+++ STL-1/string.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // int n;
+    // cin >> n;
+    // string s;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char c;
+    //     cin >> c;
+    //     s.push_back(c);
+    // }
+
+    // s.pop_back();
+    // cout << s <<'\n';
+    // cout << s.front() << " " << s.back() << endl;
+
+    // s.clear();
+    // cout << s.empty();
+    
+
+    string s;
+    cin >> s;
+    cout << s.substr(1,3)<< endl;
+    cout << s.substr(2);
+
+
+
+
+
+
+}
